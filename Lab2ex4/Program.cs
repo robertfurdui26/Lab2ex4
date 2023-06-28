0a59a79a4332acacc7453016e597588bfd3a1723
robertfurdui26 <125736128+robertfurdui26@users.noreply.github.com>
2023-06-28 15:33:29 +0300

    Add project files.

---
+++ Lab2ex4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Lab2ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* • Ex 4: Scrieti un program care va determina daca un numar este sau nu
+             palindrom.*/
+
+            //Am folosit netul,nu m-am descurcat singur,ca sa fiu sincer
+
+            Console.WriteLine("Introduceți un număr: ");
+            int numar = int.Parse(Console.ReadLine());
+
+            int numarInitial = numar;
+            int invers = 0;
+
+            while (numar > 0)
+            {
+                int cifra = numar % 10;
+                invers = invers * 10 + cifra;
+                numar /= 10;
+            }
+
+            if (numarInitial == invers)
+            {
+                Console.WriteLine("Numărul {0} este palindrom.", numarInitial);
+            }
+            else
+            {
+                Console.WriteLine("Numărul {0} nu este palindrom.", numarInitial);
+            }
+        }
+    }
+}
